63f75e375242ff65565cfb1fc94d0f9b56a01983
Alexssander Lopez <aleghost20@gmail.com>
2025-07-14 10:09:10 -0400

    Subida forzada para reemplazar el contenido del repositorio remoto

--- config/settings.py
+++ config/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.sap',
     'apps.usuarios',
     'apps.permisos',
     'apps.organizaciones',
