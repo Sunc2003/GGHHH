679034b063f6cc3e3930646097033786b38d523a
Alexssander Lopez <aleghost20@gmail.com>
2025-07-15 17:33:39 -0400

    a

--- config/urls.py
+++ config/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('usuarios/', include('apps.usuarios.urls')),
     path('', lambda request: redirect('login', permanent=False)),
-    path('permisos/', include('permisos.urls')),
+    path('permisos/', include('apps.permisos.urls')),
 ]
