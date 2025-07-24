fca8b605d4dfb1ecc4c96e243ca8b26ba45436a9
Alexssander Lopez <aleghost20@gmail.com>
2025-07-24 11:08:07 -0400

    ola

--- apps/utils/views.py
+++ apps/utils/views.py
@@ -1,32 +1,25 @@
-# apps/utils/views.py
-import os
-from django.http import JsonResponse
 from django.views import View
+from django.http import JsonResponse
 from django.core.files.base import ContentFile
-from django.core.files.storage import default_storage
 from apps.utils.supabase_storage import SupabaseStorage
 
 class TestSupabaseUploadView(View):
     def get(self, request):
         response_data = {}
+        supa = SupabaseStorage()  # 👈 usamos directamente
 
-        # Verificamos las variables de entorno
-        response_data['SUPABASE_URL'] = os.getenv('SUPABASE_URL')
-        response_data['SUPABASE_KEY_exists'] = bool(os.getenv('SUPABASE_KEY'))
-        response_data['SUPABASE_BUCKET'] = os.getenv('SUPABASE_BUCKET')
+        response_data['SUPABASE_URL'] = supa.url
+        response_data['SUPABASE_KEY_exists'] = bool(supa.key)
+        response_data['SUPABASE_BUCKET'] = supa.bucket
 
-        # Subimos archivo de prueba
         try:
             file_name = "uploads/test_render.txt"
-            file_content = ContentFile(b"Contenido de prueba desde Render")
-            path = default_storage.save(file_name, file_content)
+            file_content = ContentFile(b"Contenido desde Render")
+            supa._save(file_name, file_content)  # 👈 forzamos supabase
 
-            response_data['archivo_subido'] = path
-            response_data['url_archivo'] = default_storage.url(path)
-
-            # Validamos si el archivo existe en el bucket
-            supa = SupabaseStorage()
-            response_data['existe_en_supabase'] = supa.exists(path)
+            response_data['archivo_subido'] = file_name
+            response_data['url_archivo'] = supa.url(file_name)
+            response_data['existe_en_supabase'] = supa.exists(file_name)
 
         except Exception as e:
             response_data['error'] = str(e)
