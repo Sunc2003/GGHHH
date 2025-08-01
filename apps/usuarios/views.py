cbccd9211d0bc8e66e5be364224b0c340eb3784f
millaaaaraay <MI.ESPOSITO@DUOCUC.CL>
2025-08-01 13:36:16 -0400

    .

--- apps/usuarios/views.py
+++ apps/usuarios/views.py
@@ -310,7 +310,9 @@ def solicitudes_enviadas_view(request):
     return render(request, 'perfil.html', context)
 
 
-
+@login_required
+def procesos_view(request):
+    return render(request, 'procesos.html')
 
 
 
