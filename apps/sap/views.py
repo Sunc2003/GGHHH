5504cc6d2c1023bc4ae089af965454aea8f3cd73
Alexssander Lopez <aleghost20@gmail.com>
2025-08-08 12:36:02 -0400

    PUEDE SER AHORA SI VERSION FINAL DEFINITICA-?

--- apps/sap/views.py
+++ apps/sap/views.py
@@ -2,16 +2,26 @@ from django.shortcuts import render
 import requests
 from django.http import JsonResponse
 
-NGROK_API_URL = "https://16cbecaa20f9.ngrok-free.app/api/productos"
+NGROK_API_URL = "https://4cc96362eae8.ngrok-free.app/api/productos"
 
 def buscar_productos_remoto(request):
     termino = request.GET.get("q", "")
+    headers = {
+        "ngrok-skip-browser-warning": "true",
+        "User-Agent": "DjangoClient/1.0"
+    }
 
     try:
-        response = requests.get(NGROK_API_URL, params={"q": termino}, timeout=5)
+        response = requests.get(NGROK_API_URL, params={"q": termino}, headers=headers, timeout=5)
         response.raise_for_status()
         data = response.json()
         return JsonResponse({"productos": data})
+    except requests.exceptions.HTTPError as e:
+        return JsonResponse({
+            "error": str(e),
+            "response_text": response.text,
+            "status_code": response.status_code
+        }, status=500)
     except requests.exceptions.RequestException as e:
         return JsonResponse({"error": str(e)}, status=500)
 
